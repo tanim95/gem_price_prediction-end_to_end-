ca2e462f84d71381d636788410736c83eb149f1e
tanim95 <tanimrahman78@gmail.com>
2023-12-06 08:51:46 +0600

    .

--- application.py
+++ application.py
@@ -31,4 +31,4 @@ def prediction():
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=8000)
--- setup.py
+++ setup.py
@@ -2,19 +2,24 @@ from setuptools import find_packages, setup
 from typing import List
 
 
-def get_requirements(file_path: str) -> List[str]:
+def get_requirements(file_path):
     requirements = []
     with open(file_path) as file:
         requirements = file.readlines()
         requirements = [i.replace('\n', '') for i in requirements]
         if '-e .' in requirements:
             requirements.remove('-e .')
+    # [print(requirements)]
     return requirements
+
+
+def get_req(file):
+
 
 
 setup(
     name='gem_price_prediction',
-    version='0.0.1',
+    version='1.0.0',
     author='tanim',
     author_email='tanimrahman78@gmail.com',
     packages=find_packages(),
